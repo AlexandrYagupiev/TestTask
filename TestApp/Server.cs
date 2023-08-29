352740fc53b6dd96280f64ed17000eb7c4fe918b
Alex <siaghupiev@mail.ru>
2023-08-29 14:37:31 +0500

    Добавьте файлы проекта.

---
+++ TestApp/Server.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    static internal class Server
+    {
+        private static int count = 0;
+        private static ManualResetEvent manualResetEvent = new ManualResetEvent(false);
+        private static object lockObj = new object();
+        public static int GetCount()
+        {
+            manualResetEvent.WaitOne();
+            return count;  
+        }
+
+        public static void AddToCount(int value)
+        {
+            lock (lockObj)
+            {
+                manualResetEvent.Reset();
+                count += value;
+                manualResetEvent.Set();
+            }
+        }
+    }
+}
